fb64147330dd60b7f296757de927d34817201699
goyal.parv <goyal.parv@goyalparvs-MacBook-Pro.local>
2025-05-25 16:10:33 +0530

    Added a Footer element

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,15 @@
 import Header from "./Header.jsx"  
+import Footer from './Footer.jsx'
 
 function App() {
   return(
-    <Header/>
+    <>
+      <Header/>
+      <Footer/>
+    </>
   );
 
 }
 
 export default App
+ 
---
+++ my-react-app/src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <footer>
+            <p>&copy;{new Date().getFullYear()} Your webiste name</p>
+        </footer>
+    );
+}
+
+export default Footer; 
