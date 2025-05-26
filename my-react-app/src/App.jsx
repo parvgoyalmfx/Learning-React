661f974f6f465769038cf3a9ecda43091798aa67
goyal.parv <goyal.parv@goyalparvs-MacBook-Pro.local>
2025-05-26 15:59:00 +0530

    Starting to make a tictac toe game

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,14 +1,28 @@
 import Header from "./Header.jsx"  
 import Footer from './Footer.jsx'
 import Card from './Card.jsx'
+import Square from "./Square.jsx";
 
-function App() {
+function Board() {
   return(
     <>
-      <Card></Card>
+    <div className="board-row">
+      <Square value="X" />
+      <Square value="O" />
+      <Square value="X" />
+    </div>
+    <div className="board-row">
+      <Square value="X" />
+      <Square value="O" />
+      <Square value="X" />
+    </div>
+    <div className="board-row">
+      <Square value="X" />
+      <Square value="O" />
+      <Square value="X" />
+    </div>
     </>
   );
-
 }
 
-export default App
+export default Board
